401e9cd49d89dfe1b306da1dc98dc02ff916093c
José Junio da Silva Calú <junio.calu@gmail.com>
2024-12-13 13:52:00 +0000

    improve livebot multichannel support

--- frontend/pages/livebot.vue
+++ frontend/pages/livebot.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="flex flex-col items-center min-h-screen p-4 bg-base-200 text-base-content" :dark="colorMode.value === 'dark'">
+    <div class="w-full max-w-2xl p-4 bg-base-300 rounded-lg shadow-md text-center">
+      <h1 class="text-2xl font-bold">
+        Canal: {{ channel.name || 'Nenhum canal selecionado' }}
+      </h1>
+    </div>
     <div class="w-full max-w-2xl mt-4 p-4 bg-base-300 rounded-lg shadow-md">
       <h1 class="text-[1.16rem] font-bold mb-4 text-center">
         Clique no botão abaixo para ativar ou desativar a detecção automática de lives
@@ -21,7 +26,27 @@
 
 <script setup lang="ts">
 
+import { onBeforeUnmount, onMounted, ref, watch } from 'vue';
+
 const colorMode = useColorMode();
+const authStore = useAuth();
+const configStore = useConfig();
+
+const { i } = storeToRefs(configStore); // Índice do canal selecionado
+const channel = ref({} as Channel); // Canal atualmente selecionado
+
+// Atualiza o canal ao montar o componente e sempre que o índice mudar
+onMounted(() => {
+  updateChannel();
+});
+
+watch(i, updateChannel);
+
+function updateChannel() {
+  if (configStore.channels[i.value]) {
+    channel.value = { ...configStore.channels[i.value] };
+  }
+}
 
 useHead({
     title: 'ffplayout | Livebot'
